ee3ec54ff03e8babb83aba3b23902a3cbf8c7ed7
Rodbame <luisrodrigo.barronmejia@gmail.com>
2023-02-20 20:38:19 -0600

    mejora en vista de boton

--- src/database.js
+++ src/database.js
@@ -5,7 +5,9 @@ main().catch((err) => console.log(err));
 async function main() {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect("mongodb://127.0.0.1:27017/usuarios_bd");
+    await mongoose.connect(
+      "mongodb+srv://rodbame:crud123@crud.cznuowy.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("Connected to mongo");
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   } catch (error) {
