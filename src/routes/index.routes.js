f939befc51cae421691af647cd6190881677bc46
Alex <alexsalas0420@gmail.com>
2023-02-24 10:11:04 -0600

    UWU

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,6 @@
-import { Router } from "express";//importar 
+import { Router } from "express";//importa un metodo del modulo express 
 
+//extraes todas las funciones del controlador de la base de datos
 import {
   renderIndex,
   addUser,
@@ -8,16 +9,17 @@ import {
   deleteTask,
 } from "../controllers/task.controller";
 
+//inicializa la funcion Router en la constante router
 const router = Router();
 
-router.get("/", renderIndex);
+router.get("/", renderIndex);//trae la ruta para hacer uso de la funcion de ese archivo index
 
-router.post("/task/add", addUser);
+router.post("/task/add", addUser);//trae la ruta para hacer uso de la funcion de addUser y tomar los datos para agregar nuevos datos
 
-router.get("/edit/:id", renderTaskEdit);
+router.get("/edit/:id", renderTaskEdit);//trae la ruta para hacer uso de la funcion renderTaskEdit 
 
-router.post("/edit/:id", editTask);
+router.post("/edit/:id", editTask);//trae la ruta para hacer uso de la funcion editTask
 
-router.get("/delete/:id", deleteTask);
+router.get("/delete/:id", deleteTask);//trae la ruta para hacer uso de la funcion deleteTask
 
-export default router;
+export default router;//automaticamente exporta la funcion en especifico seleccionada, lo que se quiere realizar
